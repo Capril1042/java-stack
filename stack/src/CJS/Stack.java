23e548d4559d4d796b601db14f92aa6e09de5b8c
Capril1042 <Caprilewis1042@gmail.com>
2019-02-07 21:03:32 -0500

    cleans up code

--- stack/src/CJS/Stack.java
+++ stack/src/CJS/Stack.java
@@ -5,40 +5,55 @@ import java.util.ArrayList;
 
 public class Stack
 {
-    int max = 1000;
+    public static int max = 1000;
     int numberOfItems = 0;
-    ArrayList<String> myStack = new ArrayList<String>();
+    ArrayList<String> myStack;
 
 
     public Stack()
     {
-        this.myStack = myStack;
-        this.numberOfItems = numberOfItems;
+        this.myStack = new ArrayList<String>();
+        this.numberOfItems = 0;
+        this.max = 1000;
 
     }
 
-    public ArrayList<String> print(){
-
+    public ArrayList<String> print()
+    {
         return myStack;
     }
+
     public String pop(){
-        String s = myStack.get(myStack.size()-1);
-        myStack.remove(s);
-        this.numberOfItems = numberOfItems--;
-        return s;
+        if ( this.myStack.size() > 0)
+        {
+            String s = this.myStack.get(this.myStack.size()-1);
+            int index = this.myStack.size()-1;
+            this.myStack.remove(index);
+            this.numberOfItems = this.numberOfItems - 1;
+                return s;
+        }
+        else
+        {
+            return "Nothing left in stack";
+        }
     }
 
-    public void push(String s){
-         myStack.add(s);
-         this.numberOfItems = numberOfItems++;
+    public void push(String s)
+    {
+        if(this.numberOfItems + 1 <= this.max)
+        {
+            this.myStack.add(s);
+            this.numberOfItems = this.numberOfItems + 1;
+        }
+
     }
 
-/*    numOfItems - No items in on the stack,
-      return the message “There are no items in your Stack
-      , otherwise, return the number of items.*/
-    public String numOfItems(){
-        if(myStack.size() > 0){
-            return  Integer.toString(myStack.size());
+
+    public String numOfItems()
+    {
+        if(this.myStack.size() > 0)
+        {
+            return  Integer.toString(this.myStack.size());
         }
         return "There are no items in your Stack";
     }
